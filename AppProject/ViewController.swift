64db2266da2a2d75aed21887467dd97c9201aa5c
SamKasparyan <kasparyan.sam@gmail.com>
2021-02-01 20:09:29 +0400

    First Commit

---
+++ AppProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppProject
+//
+//  Created by Cypress on 2/1/21.
+//  Copyright © 2021 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
