972de6d4f323aaafda69f7bf4aff4dbf6c8ce4e1
SamKasparyan <kasparyan.sam@gmail.com>
2021-02-02 18:52:14 +0400

    Setting up scrollable views

---
+++ AppProject/Views/FourthHintViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FourthHintViewController.swift
+//  AppProject
+//
+//  Created by Cypress on 2/2/21.
+//  Copyright © 2021 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class FourthHintViewController: UIViewController {
+
+    @IBOutlet weak var doneButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func didTapDoneButton(_ sender: Any) {
+        
+    }
+    
+}
